070ecd9b53ad85a23df9179a11c8527e35514118
zikrulihsan <zikrulihsan2695@gmail.com>
2023-11-27 01:34:53 +0800

    fix minor bug

--- src/app/component/Modal.jsx
+++ src/app/component/Modal.jsx
@@ -6,16 +6,17 @@ const MyModal = ({ isOpen, onClose, onContinue }) => {
       <Modal isOpen={isOpen} onClose={onClose} isCentered={true} closeOnOverlayClick={false}>
         <ModalOverlay />
         <Center>
-        <ModalContent mx="4" height={"72vh"} text>
+        <ModalContent mx="4" minHeight={"72vh"} text>
           <ModalHeader fontSize={18}> &#128075; Halo naki nakwai, sebelum masuk portal, lihat ini dulu ya!</ModalHeader>
           <ModalBody>
             <Box ml="4">
                 <ul> 
-                    <li><Text fontSize={13}>Saat ini Tim Portal Sumbawa Sedang melakukan <b>Gerakan Satu Data Sumbawa</b>, mengumpulkan berbagai data penyedia jasa/produk di Sumbawa</Text></li>
-                    <li><Text fontSize={13}>Data yang tersedia di dalam Portal masih jauh dari cukup, anda bisa berpartisipasi sebagai pelaku usaha ataupun pengguna</Text></li>
-                    <li><Text fontSize={13}>Anda bisa menginputkan data sebagai penyedia jasa/produk melalui admin</Text></li>
+                    <li><Text fontSize={13}>Saat ini Tim Portal Sumbawa Sedang melakukan <b>Gerakan Satu Data Sumbawa</b>, mengumpulkan berbagai data penyedia jasa/produk di Sumbawa.</Text></li>
+                    <li><Text fontSize={13}>Data yang tersedia di dalam Portal <b>masih jauh dari cukup,</b> anda bisa berpartisipasi sebagai pelaku usaha ataupun pengguna.</Text></li>
+                    <li><Text fontSize={13}>Anda bisa menginputkan data sebagai penyedia jasa/produk melalui admin.</Text></li>
                     <li><Text fontSize={13}>Sebagai Pengguna, data yang anda cari mungkin masih belum tersedia, Anda bisa memberitahu admin tentang pencarian anda.</Text></li> 
-                    <li><Text fontSize={13}>Admin akan mengutamakan pelengkapan data dari daftar pencarian pengguna tersebut</Text> </li>
+                    <li><Text fontSize={13}>Admin akan mengutamakan pelengkapan data dari daftar pencarian pengguna tersebut.</Text> </li>
+                    <li><Text fontSize={13}>Itu saja, mohon maaf lahir batin, <b>Selamat Mencoba!</b></Text> </li>
                 </ul>
             </Box>  
             </ModalBody>
